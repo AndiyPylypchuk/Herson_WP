8348085735c46bb4bcce830410b0736fe3934754
pylypchuklena <pylypchuklena@gmail.com>
2017-06-12 12:04:02 +0300

    create main_slider and cliens

--- zakazMax/dist/js/common.js
+++ zakazMax/dist/js/common.js
@@ -108,4 +108,15 @@ $(document).ready(function(){
     infinite: false,
     adaptiveHeight: true
   });
+  $('.js-main_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    dots: false,
+    // focusOnSelect: true,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    infinite: true
+  });
 });
